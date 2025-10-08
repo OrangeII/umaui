4187a48786cdefb6f9842569ee0270f2ae128347
OrangeII <ivaldinico@gmail.com>
2025-10-08 16:30:56 +0200

    feat: add click animation

--- src/components/Button/Button.vue
+++ src/components/Button/Button.vue
@@ -5,6 +5,7 @@
       props.primary ? styles.primary : styles.neutral,
       props.small ? styles.small : null,
     ]"
+    @click="showClickAnimation"
   >
     <span :class="styles.content">
       <span v-if="$slots.icon" :class="styles.icon">
@@ -19,13 +20,31 @@
       <span :class="styles['decoration-back']"></span>
     </span>
     <span :class="styles['inner-border']"></span>
-    <ClickDecoration />
+
+    <span :class="styles['click-decoration']">
+      <!-- left decoration -->
+      <span :class="styles['left']">
+        <transition name="pop-in" @after-enter="showDecoration = false">
+          <ClickDecoration v-if="showDecoration" />
+        </transition>
+      </span>
+
+      <!-- right decoration flipped -->
+      <span :class="styles['right']">
+        <transition name="pop-in" @after-enter="showDecoration = false">
+          <ClickDecoration v-if="showDecoration" />
+        </transition>
+      </span>
+    </span>
   </button>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 import styles from "./Button.module.scss";
 import ClickDecoration from "./ButtonClickDecoration.vue";
+
+const showDecoration = ref<boolean>(false);
 
 interface ButtonProps {
   primary?: boolean;
@@ -37,4 +56,38 @@ const props = withDefaults(defineProps<ButtonProps>(), {
   small: false,
   label: "",
 });
+
+const showClickAnimation = () => {
+  setTimeout(() => {
+    showDecoration.value = true;
+  }, 100);
+};
 </script>
+
+<style scoped>
+.pop-in-enter-active {
+  animation: pop-in 0.3s linear 1;
+}
+
+@keyframes pop-in {
+  0% {
+    animation-timing-function: cubic-bezier(0.33333, 0.66667, 0.66667, 1);
+    transform: translateY(0) scale(0.6);
+    opacity: 1;
+  }
+  30% {
+    animation-timing-function: cubic-bezier(0.33333, 0, 0.66667, 0.33333);
+    transform: translate(-1rem, -1rem) scale(1);
+    opacity: 1;
+  }
+  50% {
+    animation-timing-function: cubic-bezier(0.33333, 0, 0.66667, 0.33333);
+    transform: translate(-1rem, -1rem) scale(1);
+    opacity: 1;
+  }
+  100% {
+    transform: translate(-0rem, -0rem) scale(0.4);
+    opacity: 0;
+  }
+}
+</style>
--- src/components/Button/ButtonClickDecoration.vue
+++ src/components/Button/ButtonClickDecoration.vue
@@ -1,6 +1,6 @@
 <template>
   <svg
-    :class="styles['click-decoration']"
+    :class="styles['content']"
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 48 52"
     fill="none"
