4b01b2c493bb70b89046b3c5270d662d240ec59d
OrangeII <ivaldinico@gmail.com>
2025-10-14 16:40:01 +0200

    feat: add A rank

---
+++ src/components/Rank/Rank.stories.ts
@@ -0,0 +1,74 @@
+import type { Meta, StoryObj } from "@storybook/vue3-vite";
+
+import Rank from "./Rank.vue";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+const meta = {
+  title: "Components/Rank",
+  component: Rank,
+  tags: ["autodocs"],
+  argTypes: {},
+  args: {},
+} satisfies Meta<typeof Rank>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/api/csf
+ * to learn how to use render functions.
+ */
+
+export const A: Story = {
+  args: {
+    rank: "A",
+  },
+};
+
+export const B: Story = {
+  args: {
+    rank: "B",
+  },
+};
+
+export const C: Story = {
+  args: {
+    rank: "C",
+  },
+};
+
+export const D: Story = {
+  args: {
+    rank: "D",
+  },
+};
+
+export const E: Story = {
+  args: {
+    rank: "E",
+  },
+};
+
+export const F: Story = {
+  args: {
+    rank: "F",
+  },
+};
+
+export const G: Story = {
+  args: {
+    rank: "G",
+  },
+};
+
+export const S: Story = {
+  args: {
+    rank: "S",
+  },
+};
+
+export const SS: Story = {
+  args: {
+    rank: "SS",
+  },
+};
---
+++ src/components/Rank/Rank.vue
@@ -0,0 +1,17 @@
+<template>
+  <span :class="[styles.rank, styles[props.rank]]" :data-text="props.rank">{{
+    props.rank
+  }}</span>
+</template>
+
+<script setup lang="ts">
+import styles from "./Rank.module.scss";
+
+interface RankProps {
+  rank: "A" | "B" | "C" | "D" | "E" | "F" | "G" | "S" | "SS";
+}
+
+const props = withDefaults(defineProps<RankProps>(), {
+  rank: "C",
+});
+</script>
