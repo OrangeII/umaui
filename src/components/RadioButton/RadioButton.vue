368f8ac88829e8e8bde139284665074072f2c597
OrangeII <ivaldinico@gmail.com>
2025-10-15 18:25:29 +0200

    feat: export Rank and RadioButton

--- src/components/RadioButton/RadioButton.vue
+++ src/components/RadioButton/RadioButton.vue
@@ -1,6 +1,12 @@
 <template>
   <label :class="styles.container">
-    <input type="radio" name="radio" :value="value" :class="styles.input" />
+    <input
+      type="radio"
+      name="radio"
+      :value="value"
+      :class="styles.input"
+      v-model="model"
+    />
     <span :class="styles.control"></span>
     <span :class="styles.labelText">
       <slot>{{ props.caption }}</slot>
@@ -11,12 +17,15 @@
 <script setup lang="ts">
 import styles from "./RadioButton.module.scss";
 
-const value = defineModel();
+const model = defineModel();
 
 interface RadioButtonProps {
   caption?: string;
+  value?: any;
 }
+
 const props = withDefaults(defineProps<RadioButtonProps>(), {
   caption: "",
+  value: null,
 });
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
 export { default as Button } from "./components/Button/Button.vue";
 export { default as DialogChoice } from "./components/DialogChoice/DialogChoice.vue";
 export { default as Table } from "./components/Table/Table.vue";
+export { default as Rank } from "./components/Rank/Rank.vue";
+export { default as RadioButton } from "./components/RadioButton/RadioButton.vue";
