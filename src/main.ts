4f442eef9b8da16b1eb5afb1383ec50076b6cb8e
OrangeII <ivaldinico@gmail.com>
2025-10-18 12:20:26 +0200

    feat: rename RadioButton to Radio

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,4 @@ export { default as Button } from "./components/Button/Button.vue";
 export { default as DialogChoice } from "./components/DialogChoice/DialogChoice.vue";
 export { default as Table } from "./components/Table/Table.vue";
 export { default as Rank } from "./components/Rank/Rank.vue";
-export { default as Radio } from "./components/RadioButton/Radio.vue";
+export { default as Radio } from "./components/Radio/Radio.vue";
