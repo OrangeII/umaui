b08ba2901bc47ab8478efc9aea780286760b7d05
OrangeII <ivaldinico@gmail.com>
2025-10-22 11:36:08 +0200

    refactor: cleat icon

--- src/components/DialogChoice/DialogChoice.vue
+++ src/components/DialogChoice/DialogChoice.vue
@@ -5,17 +5,7 @@
   >
     <span :class="styles.overlay"></span>
     <span :class="styles.inner">
-      <svg
-        width="25"
-        :class="styles.icon"
-        viewBox="0 0 25 29"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M3.5 28.5L0 24.5L2.5 22.5C-1.16667 15.1667 -2.49998 0.5 12.5 0.5C27.5 0.5 26.1667 15.1667 22.5 22.5L25 24.5L22 28.5L15.5 24C18.5 18.5 20.5 7.99999 12.5 7.99999C4.49999 7.99999 6.5 18.8333 9.5 24L3.5 28.5Z"
-        />
-      </svg>
+      <Cleat width="25" :class="styles.icon" />
 
       <span :class="styles.text">
         <slot>
@@ -43,6 +33,7 @@
 import styles from "./DialogChoice.module.scss";
 import ButtonClickDecoration from "../ButtonClickDecoration/ButtonClickDecoration.vue";
 import { useButtonClickDecoration } from "../ButtonClickDecoration/buttonClickDecoration";
+import Cleat from "../Icons/Cleat.vue";
 
 const { showDecoration, triggerDecoration } = useButtonClickDecoration();
 
